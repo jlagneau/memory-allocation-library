d923e1106b0dca89d43342346e2bf9acb082122d
jlagneau <jlagneau@student.42.fr>
2016-09-08 15:58:14 +0200

    bzero the newly reclaimed memory page

--- src/create_alloc.c
+++ src/create_alloc.c
@@ -6,7 +6,7 @@
 /*   By: jlagneau <jlagneau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/08 10:36:16 by jlagneau          #+#    #+#             */
-/*   Updated: 2016/09/08 10:40:07 by jlagneau         ###   ########.fr       */
+/*   Updated: 2016/09/08 15:55:37 by jlagneau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ void 	*create_alloc(size_t size)
 {
 	int		prot;
 	int		flags;
+	void	*tmp;
 
 	prot = PROT_READ | PROT_WRITE;
 	flags = MAP_ANON | MAP_PRIVATE;
-	return (mmap(NULL, size, prot, flags, -1, 0));
+	tmp = mmap(NULL, size, prot, flags, -1, 0);
+	ft_bzero(tmp, size);
+	return (tmp);
 }
